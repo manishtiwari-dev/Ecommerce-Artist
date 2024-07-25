da2c430bb9a3070f55c58a38aee34c0a1ef8e980
Manish Tiwari <95132186+manishtiwari-dev@users.noreply.github.com>
2024-07-25 14:37:35 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,32 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+    images: { unoptimized: true },
+    //images: { unoptimized: true } ,
+    // useFileSystemPublicRoutes: false,
+
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+   
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+   
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'build',
+  }
+   
+  module.exports = nextConfig
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: 'export',
+//     images: { unoptimized: true } ,
+   
+// }
+
+// export default nextConfig;
+
+
